8f454579efd9b3e530622cce229f4a1a813b7aff
Satont <satontworldwide@gmail.com>
2026-04-04 03:29:27 +0300

    fix: corrcet resolve

--- packages/desktop/vite.main.config.ts
+++ packages/desktop/vite.main.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+const __dirname = import.meta.dirname
+
 export default defineConfig({
   build: {
     emptyOutDir: true,
@@ -10,11 +12,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@desktop': resolve(__dirname, 'src'),
-      '@twirchat/shared': resolve(__dirname, '../shared/index.ts'),
+      '@twirchat/shared/types': resolve(__dirname, '../shared/types.ts'),
       '@twirchat/shared/constants': resolve(__dirname, '../shared/constants.ts'),
       '@twirchat/shared/protocol': resolve(__dirname, '../shared/protocol.ts'),
-      '@twirchat/shared/types': resolve(__dirname, '../shared/types.ts'),
+      '@twirchat/shared': resolve(__dirname, '../shared/index.ts'),
+      '@desktop': resolve(__dirname, 'src'),
     },
   },
   root: 'src/views/main',
